3c9e410811f3becd61a3418df55f05d0d8997c13
Radiquum <kentai.waah@gmail.com>
2025-09-19 18:15:42 +0500

    add utils

---
+++ scripts/utils.py
@@ -0,0 +1,12 @@
+def run_cmd(cmd: list[str], ignore_error: bool = False) -> bool | None:
+    pass
+
+
+def read_apktool_yml() -> tuple[str, int, int, int]:
+    pass
+
+
+def save_apktool_yml(
+    versionName: str, versionCode: int, minSdkVersion: int, targetSdkVersion: int
+) -> None:
+    pass
